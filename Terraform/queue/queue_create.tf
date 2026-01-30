bee7ac3a000214b516fae40acbc6c22af28f2bfd
Praneeth3394 <mandali.praneeth@gmail.com>
2026-01-30 13:30:19 +0530

    Update queue_create.tf

--- Terraform/queue/queue_create.tf
+++ Terraform/queue/queue_create.tf
@@ -1,42 +1,68 @@
-resource "genesyscloud_routing_queue" "Genesys_Support_Queue" {
-  name = "Genesys_Support_Queue"
-  media_settings_callback {
+resource "genesyscloud_routing_queue" "my_queue" {
+
+  name                      = "Support_queue_genesys"
+
+  description               = "Queue for customer support"
+ 
+media_settings_callback {
+
     enable_auto_answer        = false
+
     mode                      = "AgentFirst"
+
     alerting_timeout_sec      = 30
+
     auto_end_delay_seconds    = 300
+
     enable_auto_dial_and_end  = false
+
     service_level_duration_ms = 20000
+
     service_level_percentage  = 0.8
+
     auto_dial_delay_seconds   = 300
+
   }
+
   media_settings_chat {
-    enable_auto_answer        = false
-    enable_auto_dial_and_end  = false
+
     service_level_duration_ms = 20000
+
     service_level_percentage  = 0.8
+
     alerting_timeout_sec      = 30
+
   }
+
   media_settings_message {
+
     service_level_duration_ms = 20000
+
     service_level_percentage  = 0.8
+
     alerting_timeout_sec      = 30
-    enable_auto_answer        = false
-    enable_auto_dial_and_end  = false
+
   }
+
   media_settings_call {
+
     service_level_percentage  = 0.8
+
     alerting_timeout_sec      = 8
-    enable_auto_answer        = false
-    enable_auto_dial_and_end  = false
+
     service_level_duration_ms = 20000
+
   }
+
   media_settings_email {
-    enable_auto_dial_and_end  = false
+
     service_level_duration_ms = 86400000
+
     service_level_percentage  = 0.8
+
     alerting_timeout_sec      = 300
-    enable_auto_answer        = false
+
   }
-  ...
+
 }
+ 
